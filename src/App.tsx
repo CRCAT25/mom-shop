d1cb32ddf98c02fff4504b45f50be0661571dcf0
thanhdev <>
2025-12-14 18:39:03 +0700

    complete Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Cart from './pages/Cart/Cart';
 import Menu from './pages/Menu/Menu';
 import ProductList from './pages/Product/ProductList';
 import type { ProductCategoryDTO } from './types/ProductCategoryDTO';
+import Header from './pages/Layout/Header';
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState<ProductCategoryDTO | null>(null);
@@ -11,13 +12,15 @@ function App() {
   return (
     <div className='bg-linear-to-br from-white to-[#FFF9F0]'>
       {/* HEADER */}
-      <div className="header h-[110px]">
-
+      <div className="sticky top-0 z-10">
+        <Header />
       </div>
 
       {/* CONTENT */}
-      <div className='flex gap-10 justify-center h-auto items-start'>
-        <Menu onSelect={setSelectedCategory} />
+      <div className='flex gap-10 justify-center h-auto items-start py-4 px-10 max-w-8xl'>
+        <div className='w-full max-w-[320px] sticky min-w-[200px] top-24'>
+          <Menu onSelect={setSelectedCategory} />
+        </div>
         <ProductList category={selectedCategory} />
         <Cart />
       </div>
---
+++ src/pages/Layout/Header.tsx
@@ -0,0 +1,33 @@
+const Header = () => {
+    return (
+        <header className="bg-white border-b border-gray-200 shadow-sm">
+            <div className="max-w-8xl mx-auto px-10 py-4 flex items-center justify-between">
+                {/* Logo */}
+                <div className="flex items-center">
+                    <h1 className="text-5xl italic text-gray-800" style={{ fontFamily: 'Playball, cursive' }}>
+                        Cô Thoa Quán
+                    </h1>
+                </div>
+
+                {/* Contact & Auth */}
+                <div className="flex items-center gap-6">
+                    <div className="text-right">
+                        <p className="text-red-600 text-sm font-semibold">Liên hệ trực tiếp</p>
+                        <p className="text-gray-800 font-bold text-lg">0935.551.538</p>
+                    </div>
+
+                    <div className="flex gap-3">
+                        <button className="cursor-pointer px-6 py-2 border-2 border-[#ffb432] text-[#ffb432] font-semibold rounded-full hover:bg-orange-50 transition-all duration-300">
+                            Đăng ký
+                        </button>
+                        <button className="cursor-pointer px-6 py-2 bg-linear-to-r from-amber-500 to-[#ffb432] text-white font-semibold rounded-full hover:shadow-lg hover:scale-105 transition-all duration-300">
+                            Đăng nhập
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
